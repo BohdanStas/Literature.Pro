607e305b6ba698265dfb9bb1dc274fd4f9bc05c5
Bohdan <Bohdan@DESKTOP-ESMFU6D>
2018-11-26 16:39:49 +0200

    Added db

--- Literature.Pro/Form1.Designer.cs
+++ Literature.Pro/Form1.Designer.cs
@@ -32,8 +32,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.booksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bookAuthorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.favoriteBooksToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookAuthorsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.видавництваToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видачаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.читачToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.historyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutAProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,7 +46,23 @@
             this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.librarydbDataSet = new Literature.Pro.LibrarydbDataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.booksBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.booksTableAdapter = new Literature.Pro.LibrarydbDataSetTableAdapters.booksTableAdapter();
+            this.idbookDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namebookDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.autorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.editionyearDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.countDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idPublishingDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.librarydbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -52,8 +70,6 @@
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.booksToolStripMenuItem,
-            this.bookAuthorsToolStripMenuItem,
-            this.favoriteBooksToolStripMenuItem,
             this.historyToolStripMenuItem,
             this.helpToolStripMenuItem,
             this.aboutAProgramToolStripMenuItem});
@@ -65,24 +81,43 @@
             // 
             // booksToolStripMenuItem
             // 
+            this.booksToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bookAuthorsToolStripMenuItem1,
+            this.видавництваToolStripMenuItem,
+            this.видачаToolStripMenuItem,
+            this.читачToolStripMenuItem});
             this.booksToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("booksToolStripMenuItem.Image")));
             this.booksToolStripMenuItem.Name = "booksToolStripMenuItem";
-            this.booksToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
-            this.booksToolStripMenuItem.Text = "Book list";
+            this.booksToolStripMenuItem.Size = new System.Drawing.Size(118, 24);
+            this.booksToolStripMenuItem.Text = "База даних";
             // 
-            // bookAuthorsToolStripMenuItem
+            // bookAuthorsToolStripMenuItem1
             // 
-            this.bookAuthorsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("bookAuthorsToolStripMenuItem.Image")));
-            this.bookAuthorsToolStripMenuItem.Name = "bookAuthorsToolStripMenuItem";
-            this.bookAuthorsToolStripMenuItem.Size = new System.Drawing.Size(128, 24);
-            this.bookAuthorsToolStripMenuItem.Text = "Book authors";
+            this.bookAuthorsToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("bookAuthorsToolStripMenuItem1.Image")));
+            this.bookAuthorsToolStripMenuItem1.Name = "bookAuthorsToolStripMenuItem1";
+            this.bookAuthorsToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.bookAuthorsToolStripMenuItem1.Text = "Книги";
             // 
-            // favoriteBooksToolStripMenuItem
+            // видавництваToolStripMenuItem
             // 
-            this.favoriteBooksToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("favoriteBooksToolStripMenuItem.Image")));
-            this.favoriteBooksToolStripMenuItem.Name = "favoriteBooksToolStripMenuItem";
-            this.favoriteBooksToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
-            this.favoriteBooksToolStripMenuItem.Text = "Favorite books";
+            this.видавництваToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("видавництваToolStripMenuItem.Image")));
+            this.видавництваToolStripMenuItem.Name = "видавництваToolStripMenuItem";
+            this.видавництваToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.видавництваToolStripMenuItem.Text = "Видавництва ";
+            // 
+            // видачаToolStripMenuItem
+            // 
+            this.видачаToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("видачаToolStripMenuItem.Image")));
+            this.видачаToolStripMenuItem.Name = "видачаToolStripMenuItem";
+            this.видачаToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.видачаToolStripMenuItem.Text = "Видачі";
+            // 
+            // читачToolStripMenuItem
+            // 
+            this.читачToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("читачToolStripMenuItem.Image")));
+            this.читачToolStripMenuItem.Name = "читачToolStripMenuItem";
+            this.читачToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.читачToolStripMenuItem.Text = "Читачі";
             // 
             // historyToolStripMenuItem
             // 
@@ -107,7 +142,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 343);
+            this.button1.Location = new System.Drawing.Point(23, 331);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 44);
             this.button1.TabIndex = 2;
@@ -116,7 +151,7 @@
             // 
             // add
             // 
-            this.add.Location = new System.Drawing.Point(202, 343);
+            this.add.Location = new System.Drawing.Point(128, 331);
             this.add.Name = "add";
             this.add.Size = new System.Drawing.Size(99, 44);
             this.add.TabIndex = 3;
@@ -125,7 +160,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(394, 343);
+            this.button3.Location = new System.Drawing.Point(255, 331);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(99, 44);
             this.button3.TabIndex = 4;
@@ -156,12 +191,93 @@
             this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = this.librarydbDataSet;
+            this.bindingSource1.Position = 0;
+            // 
+            // librarydbDataSet
+            // 
+            this.librarydbDataSet.DataSetName = "LibrarydbDataSet";
+            this.librarydbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idbookDataGridViewTextBoxColumn,
+            this.namebookDataGridViewTextBoxColumn,
+            this.autorDataGridViewTextBoxColumn,
+            this.editionyearDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.countDataGridViewTextBoxColumn,
+            this.idPublishingDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.booksBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(958, 272);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // booksBindingSource
+            // 
+            this.booksBindingSource.DataMember = "books";
+            this.booksBindingSource.DataSource = this.bindingSource1;
+            // 
+            // booksTableAdapter
+            // 
+            this.booksTableAdapter.ClearBeforeFill = true;
+            // 
+            // idbookDataGridViewTextBoxColumn
+            // 
+            this.idbookDataGridViewTextBoxColumn.DataPropertyName = "idbook";
+            this.idbookDataGridViewTextBoxColumn.HeaderText = "idbook";
+            this.idbookDataGridViewTextBoxColumn.Name = "idbookDataGridViewTextBoxColumn";
+            // 
+            // namebookDataGridViewTextBoxColumn
+            // 
+            this.namebookDataGridViewTextBoxColumn.DataPropertyName = "Name_book";
+            this.namebookDataGridViewTextBoxColumn.HeaderText = "Name_book";
+            this.namebookDataGridViewTextBoxColumn.Name = "namebookDataGridViewTextBoxColumn";
+            // 
+            // autorDataGridViewTextBoxColumn
+            // 
+            this.autorDataGridViewTextBoxColumn.DataPropertyName = "Autor";
+            this.autorDataGridViewTextBoxColumn.HeaderText = "Autor";
+            this.autorDataGridViewTextBoxColumn.Name = "autorDataGridViewTextBoxColumn";
+            // 
+            // editionyearDataGridViewTextBoxColumn
+            // 
+            this.editionyearDataGridViewTextBoxColumn.DataPropertyName = "Edition_year";
+            this.editionyearDataGridViewTextBoxColumn.HeaderText = "Edition_year";
+            this.editionyearDataGridViewTextBoxColumn.Name = "editionyearDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // countDataGridViewTextBoxColumn
+            // 
+            this.countDataGridViewTextBoxColumn.DataPropertyName = "Count";
+            this.countDataGridViewTextBoxColumn.HeaderText = "Count";
+            this.countDataGridViewTextBoxColumn.Name = "countDataGridViewTextBoxColumn";
+            // 
+            // idPublishingDataGridViewTextBoxColumn
+            // 
+            this.idPublishingDataGridViewTextBoxColumn.DataPropertyName = "IdPublishing";
+            this.idPublishingDataGridViewTextBoxColumn.HeaderText = "IdPublishing";
+            this.idPublishingDataGridViewTextBoxColumn.Name = "idPublishingDataGridViewTextBoxColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.add);
             this.Controls.Add(this.button1);
@@ -170,8 +286,13 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Literature.Pro";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.librarydbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.booksBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -181,8 +302,6 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem booksToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem bookAuthorsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem favoriteBooksToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem historyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutAProgramToolStripMenuItem;
@@ -193,6 +312,22 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ToolStripMenuItem bookAuthorsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem видавництваToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видачаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem читачToolStripMenuItem;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private LibrarydbDataSet librarydbDataSet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource booksBindingSource;
+        private LibrarydbDataSetTableAdapters.booksTableAdapter booksTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idbookDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namebookDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn autorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn editionyearDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn countDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idPublishingDataGridViewTextBoxColumn;
     }
 }
 
--- Literature.Pro/Form1.cs
+++ Literature.Pro/Form1.cs
@@ -17,6 +17,11 @@ namespace Literature.Pro
             InitializeComponent();
         }
 
-   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'librarydbDataSet.books' table. You can move, or remove it, as needed.
+            this.booksTableAdapter.Fill(this.librarydbDataSet.books);
+
+        }
     }
 }
